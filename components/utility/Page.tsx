9f3e63f411f9e1bf9dd4ead510966168a61a171b
VSLuis01 <leoboyluis143@gmail.com>
2023-10-26 00:17:52 +0530

    main page

---
+++ components/utility/Page.tsx
@@ -0,0 +1,74 @@
+import Footer from "../global/Footer";
+import Head from "next/head";
+import MobileNavbar from "../global/MobileNavbar";
+import Navbar from "../global/Navbar";
+import React, { ReactChildren } from "react";
+
+function Page({ currentPage, meta: { title, desc }, children }: PageProps) {
+  const pageTitle = `${
+    currentPage === "Home"
+      ? "VSx"
+      : `${currentPage} - vsluis01`
+  }`;
+  console.log(currentPage);
+  return (
+    <div
+      className="w-full m-auto flex flex-col items-center justify-center min-h-screen opening-box-animate-paddin text-white overflow-hidden md:overflow-visible"
+      style={{ maxWidth: "1200px" }}
+    >
+      <Head>
+        <title>{pageTitle}</title>
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/static/favicon/favicon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/favicon/favicon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/favicon/favicon.png"
+        />
+        <link rel="manifest" href="/static/favicon/site.webmanifest" />
+        <meta name="title" content={pageTitle} />
+        <meta name="description" content={desc} />
+      
+      </Head>
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KC3CN7V"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        }}
+      ></noscript>
+
+      <main className="p-5 w-full flex-1 text-center">
+        <div className="hidden sm:block z-100">
+          <Navbar currentPage={currentPage} />
+        </div>
+        <div className="-m-5 block sm:hidden z-100">
+          <MobileNavbar />
+        </div>
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Page;
+
+type PageProps = {
+  currentPage: string;
+  meta: {
+    title?: string;
+    desc: string;
+  };
+  children?: JSX.Element | JSX.Element[];
+};
